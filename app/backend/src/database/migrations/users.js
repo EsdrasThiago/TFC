82ee8c70bfb4a37ac0a85206dc88a7a7a819831a
Esdras <esdras12thiago@gmail.com>
2023-02-28 12:42:25 -0300

    Model e Migration de User criada

---
+++ app/backend/src/database/migrations/users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      }
+    });
+},
+
+down: (queryInterface) => {
+   return queryInterface.dropTable('users');
+}
+};
---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,39 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  // declare <campo>: <tipo>;
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+  username: STRING,
+  role: STRING,
+  email: STRING,
+  password: STRING,
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default User;
